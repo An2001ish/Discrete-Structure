408dc0d631be8129d15172862c3bf5cadabc25b2
Anish Baraily <84216008+An2001ish@users.noreply.github.com>
2022-01-15 20:09:58 +0545

    Add files via upload

---
+++ LAB 1/main.cpp
@@ -0,0 +1,41 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int arr1[3],arr2[3],arr3[3];
+    int i,j,k=0;
+
+    for(i=0;i<3;i++){
+
+        cout<<"arr1 element "<<i<<" :";
+        cin>>arr1[i];
+
+    }
+
+    cout<<endl;
+
+    for(i=0;i<3;i++){
+
+        cout<<"arr2 element "<<i<<" :";
+        cin>>arr2[i];
+
+    }
+
+    for(i=0;i<3;i++){
+        for(j=0;j<3;j++){
+
+
+
+        }
+    }
+
+    for(i=0;i<k;i++){
+
+        cout<<"Element "<<i<<" :"<<arr3[i]<<endl;
+
+    }
+
+ return 0;
+}
