1120d42ed4258ba2bf8857f139214f8d95f0d398
Anish Baraily <84216008+An2001ish@users.noreply.github.com>
2022-01-17 08:24:19 +0545

    Add files via upload

---
+++ LAB 1/floorceil.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+#include<cmath>
+
+using namespace std;
+
+int main()
+{
+    float a = 2.4;
+
+    cout<<"Its floor is: "<<floor(a)<<endl;
+    cout<<"Its ceil is: "<<ceil(a);
+
+    return 0;
+}
